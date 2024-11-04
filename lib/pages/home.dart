eb01c0a704f175b5d15889eeaff9558f6b66980e
manimaduri <manimaduri2@gmail.com>
2024-11-04 16:49:15 +0530

    gesture detector to buttons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,19 +7,56 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       //appbar
-      appBar: AppBar(
-        title: Text(
-          'Home Page',
-          style: TextStyle(
+      appBar: appBar(context),
+    );
+  }
+
+  AppBar appBar(BuildContext context) {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      elevation: 0,
+      leading: GestureDetector(
+        onTap: () => {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const Icon(
+            Icons.arrow_back,
             color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
           ),
         ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        elevation: 0,
       ),
+      actions: [
+        GestureDetector(
+          onTap: () => {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 40,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
